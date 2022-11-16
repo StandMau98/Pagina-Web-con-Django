9f5434386d8bc632c1bb4941c398821a66ea3079
Mauro <mau1408@outlook.com>
2022-11-16 14:15:17 -0300

    Se agregaro modelo al DB

--- Modelos/views.py
+++ Modelos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.template import Template
+from Modelos.models import Ventanas
 
 # Create your views here.
 def inicio(request):
     return render(request,"Modelos/index.html")
+
