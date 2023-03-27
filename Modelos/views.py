5d68af35e7d1f1f86c18e0f725392f113149a54b
mauro <mauro@MacBook-Pro-de-WS02.local>
2023-03-27 18:34:24 -0300

    actualizacion

--- Modelos/views.py
+++ Modelos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.template import Template
 from Modelos.models import *
 
+
 # Create your views here.
 def inicio(request):
     return render(request,"Modelos/index.html")
